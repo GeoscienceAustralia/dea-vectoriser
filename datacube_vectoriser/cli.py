40e3e5985bb3ce9fce191515b1f21affe1d9e724
Damien Ayers <damien@omad.net>
2021-06-01 14:32:01 +1000

    initial commit

---
+++ datacube_vectoriser/cli.py
@@ -0,0 +1,40 @@
+import boto3
+import click
+
+
+@click.command()
+@click.argument('queue_name')
+def process_sqs_messages(queue_name):
+
+
+    pass
+
+def receive_messages(queue_url):
+    sqs = boto3.client('sqs')
+
+    # Receive message from SQS queue
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=0
+    )
+
+    message = response['Messages'][0]
+    receipt_handle = message['ReceiptHandle']
+
+    # Delete received message from queue
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
+    print('Received and deleted message: %s' % message)
+
+if __name__ == '__main__':
+    process_sqs_messages()
